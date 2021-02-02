ac66ed06bc8f90ef0282591b9525c7e3ee748ff9
jwilson42 <wilson.jd022@gmail.com>
2021-02-02 10:22:19 -0800

    Set up a person class

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 const express = require("express");
 const app = express();
+const Person = require("./person");
+
+// const leo = new Person("Leo", true);
+// const ichigo = new Person("Kurosaki, Ichigo", false);
+
+// ichigo.greet();
+// ichigo.eat("Ocha");
+// ichigo.fightBattle = function() {
+//     console.log(`${this.name}: I fight for my friends!`);
+
+//     if (this.losingTheFight) {
+//         ichigo.usePowerOfFriendship();
+//     } else {
+//         ichigo.throwTheFight();
+//     }
+// }
+
+// leo.greet();
+// leo.walk();
+// leo.powerLevel = function() {
+//     console.log(`${this.name}: Power level over 9000`);
+// }
 
 app.get('/', (req, res) => {
     res.send("You made it");
---
+++ person.js
@@ -0,0 +1,30 @@
+module.exports = class Person {
+    constructor(name, isEngineer) {
+        this.name = name;
+        this.isEngineer = isEngineer;
+        this.walking = false;
+    }
+
+    /**
+     * Prints to the console a greeting
+     */
+    greet() {
+        console.log(`Hi my name is, ${this.name}`);
+    }
+
+    /**
+     * Prints to the console that the person is now walking
+     */
+    walk() {
+        this.walking = true;
+        console.log(`${this.name}: time for a walk.`);
+    }
+
+    /**
+     * prints to the console the food the person is eatting.
+     * @param {string} food - A string representing food to eat.
+     */
+    eat(food) {
+        console.log(`${this.name}: Time to eat some ${food}.`)
+    }
+}
